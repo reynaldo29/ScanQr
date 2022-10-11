2c508f5c18d2d046e155a88c8934d5c0d656b3d5
reynaldo29 <acm19@outlook.com>
2022-10-11 13:19:33 -0500

    Agregar archivos de proyecto.

---
+++ ScanQr/ScanQr/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
+
+namespace ScanQr
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushModalAsync(scan);
+
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopModalAsync();
+                    await DisplayAlert("Valor de QR", "" + result.Text, "OK");
+                });
+            };
+        }
+    }
+}
